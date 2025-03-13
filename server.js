f7d224d91484a1c96c433efaba9191c94180fd29
00Thor <paubiaksang@frameimpacts.com>
2025-03-13 14:00:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,22 +10,11 @@ const upload = require("./middleware/multerConfig");
 
 const app = express();
 const server = http.createServer(app);
-const allowedOrigins = [
-  "https://admin-ccpur.netlify.app",
-];
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-}));
+app.use(cors());
 
 app.use(helmet()); // Add security headers
 
